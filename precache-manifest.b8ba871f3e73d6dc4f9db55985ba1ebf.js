1de5135cf026b0661b024e11fecf6b3d215ad6f7
Harry Man <harryman@Harrys-MacBook-Air-3.local>
2020-04-04 10:28:09 +0100

    Updates

---
+++ precache-manifest.b8ba871f3e73d6dc4f9db55985ba1ebf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "443a372dcf62662f1b554db504ff6372",
+    "url": "/harrymandeveloper.github.io/tabletennistournamentmaker/index.html"
+  },
+  {
+    "revision": "aa914ff1b484ddb1c5eb",
+    "url": "/harrymandeveloper.github.io/tabletennistournamentmaker/static/css/main.ceeae9d3.chunk.css"
+  },
+  {
+    "revision": "2aa2b81658884e62f3c2",
+    "url": "/harrymandeveloper.github.io/tabletennistournamentmaker/static/js/2.34fb6334.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/harrymandeveloper.github.io/tabletennistournamentmaker/static/js/2.34fb6334.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aa914ff1b484ddb1c5eb",
+    "url": "/harrymandeveloper.github.io/tabletennistournamentmaker/static/js/main.1113264a.chunk.js"
+  },
+  {
+    "revision": "2f8457f6fe0004ea3c63",
+    "url": "/harrymandeveloper.github.io/tabletennistournamentmaker/static/js/runtime-main.99bd2801.js"
+  }
+]);
